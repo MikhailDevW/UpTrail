9f035cf4652d5d9550babf0d29e954cf9a261054
MikhailDevW <wildmv@gmail.com>
2024-03-09 13:33:34 +0300

    [UT-10] Добавлена валидация gpx трека.

--- backend/tracks/router.py
+++ backend/tracks/router.py
@@ -1,10 +1,13 @@
+import os
 import logging
+from pathlib import Path
 
 from fastapi import APIRouter, UploadFile, File, status
 from fastapi.templating import Jinja2Templates
-from pathlib import Path
+from lxml import etree
 
 from .tracks_manager import TrackManager
+from config import BASE_DIR
 
 TEMPLATE_DIR = Path(__file__).parent.parent.parent.resolve()
 
@@ -30,9 +33,7 @@ async def get_tracks(lt_lat=55.4, lt_long=43.4, rb_lat=54, rb_long=42):
 
 
 @router.post("/post_track", status_code=status.HTTP_201_CREATED)
-async def post_track(
-    track_file: UploadFile = File()
-):
+async def post_track(track_file: UploadFile = File(...)):
     """
     Публикация нового трека.
     Пользователь загружает один файл своего пройденого трека.
@@ -50,3 +51,30 @@ async def post_track(
         "longitute": parse_track.get_general_info()[1],
         "distance": parse_track.get_general_info()[2],
     }
+
+
+@router.post("/validate_xml/")
+async def upload_xml(file: UploadFile = File(...)):
+    """
+    Некий такой сервисный урл на время для проверки трееков.
+    Проверяем заранее валидный трек, проходит ли он проверку или нет.
+    Ну и можно использовать для настрйоки шаблона валидации.
+    """
+    # Сохраняем загруженный файл во временный файл
+    with open(f"temp_{file.filename}", "wb") as temp_file:
+        temp_file.write(await file.read())
+
+    # Читаем данные из временного файла и создаем XML дерево
+    tree = etree.parse(temp_file.name)
+    root = tree.getroot()
+
+    # Определяем схему XSD
+    valid_schema = BASE_DIR / "gps_test_files/uptrail_custom.xsd"
+    schema = etree.XMLSchema(file=valid_schema)
+
+    # Проверяем соответствие XML данных схеме XSD
+    if not schema.validate(root):
+        os.remove(BASE_DIR / f"temp_{file.filename}")
+        return {"message": "XML data is not valid."}
+    os.remove(BASE_DIR / f"temp_{file.filename}")
+    return {"message": "XML data is valid."}
