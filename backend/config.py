dcc9530276091c494db6623aaa6fa8f15ebb18ae
MikhailDevW <wildmv@gmail.com>
2024-04-04 20:41:47 +0300

    [UT-14] add async db operations.

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,18 @@
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).parent.resolve()
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+API_NAME = "Track"
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 8000
---
+++ backend/database.py
@@ -0,0 +1,64 @@
+import asyncio
+
+from sqlalchemy import select, text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from tracks.models import Base, TrackModel
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD
+
+
+def database_url_async():
+    return (
+        f"postgresql+asyncpg:"
+        f"//{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+
+async_engine = create_async_engine(
+    url=database_url_async(),
+    echo=True,
+    pool_size=5,
+)
+
+
+track_session = async_sessionmaker(bind=async_engine)
+
+
+async def get_db_version():
+    async with async_engine.connect() as db_connection:
+        res = await db_connection.execute(text("SELECT VERSION()"))
+        print(res.all())
+
+
+async def add_track(
+    model_instance,
+    lt_lat,
+    lt_long,
+    rb_lat,
+    rb_long,
+    session=track_session
+):
+    """Add new track ro database."""
+    async with session() as async_session:
+        async_session.add(model_instance)
+        await async_session.commit()
+
+
+async def get_all_tracks(
+    lt_lat, lt_long, rb_lat, rb_long, session=track_session
+):
+    """Get all records from database."""
+    async with session() as async_session:
+        stmt = select(TrackModel)
+        result = await async_session.execute(stmt)
+        tracks = result.scalars().all()
+        return tracks
+
+# test cases
+if __name__ == "__main__":
+    async def test_case_del_and_create():
+        async with async_engine.begin() as connect:
+            await connect.run_sync(Base.metadata.drop_all)
+            await connect.run_sync(Base.metadata.create_all)
+
+    asyncio.run(test_case_del_and_create())
